990f67806a7f1b7198887581ea71f75fde136874
Santiago Avila <sanavila2002@gmail.com>
2022-02-01 15:54:18 -0500

    Buttons were implement to choose the mode

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
 const squareDivs = document.querySelector('.squareDivs');
+const rainbowbtn = document.querySelector('.rainbowbtn')
+const blackbtn = document.querySelector('.blackbtn')
+const eraserbtn = document.querySelector('.eraserbtn')
+const cleanbtn = document.querySelector('.cleanbtn')
 
 let quantify = prompt("De cuanto quieres que sea la cuadricula")
 
 function createDivs() {
-    let totalOfDivs = (quantify * quantify) ;
+    let totalOfDivs = (quantify * quantify);
     for (i = 0; i < `${totalOfDivs}`; i++) {
         const divs = document.createElement('div');
         divs.classList.add('gridDivs');
@@ -12,35 +16,62 @@ function createDivs() {
     squareDivs.setAttribute('style', `grid-template-columns: repeat(${quantify}, 1fr)`)
 }
 
-function paintBlack () {
+function paintBlack() {
     const divsBlack = document.querySelectorAll('.gridDivs')
     divsBlack.forEach(gridDivs => {
         gridDivs.addEventListener('mouseover', () => {
-            gridDivs.classList.add('paintBlack')
+            gridDivs.setAttribute('style', 'background-color: black')
         })
     })
 }
 
-function eraser () {
+function eraser() {
     const divsBlack = document.querySelectorAll('.gridDivs')
     divsBlack.forEach(gridDivs => {
         gridDivs.addEventListener('mouseover', () => {
-            gridDivs.classList.remove('paintBlack')
-            gridDivs.classList.remove('rainbowColor')
+            gridDivs.setAttribute('style', 'background-color: whitesmoke;')
         })
     })
 }
 
-function rainbowColor () {
+function rainbowColor() {
     const divsBlack = document.querySelectorAll('.gridDivs')
     divsBlack.forEach(gridDivs => {
         gridDivs.addEventListener('mouseover', () => {
-            const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            gridDivs.setAttribute('style' , `background-color: #${randomColor}`)
+            const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            gridDivs.setAttribute('style', `background-color: #${randomColor}`)
         })
     })
 }
+
+function clean () {
+    const divsBlack = document.querySelectorAll('.gridDivs')
+    divsBlack.forEach(gridDivs => {
+        gridDivs.setAttribute('style', 'background-color: whitesmoke;')
+    })
+}
+
+function selectOption() {
+    rainbowbtn.addEventListener('click', () => {
+        rainbowColor();
+    })
+    eraserbtn.addEventListener('click', () => {
+        eraser();
+    })
+    blackbtn.addEventListener('click', () => {
+        paintBlack();
+    })
+    cleanbtn.addEventListener('click', () => {
+        clean();
+    })
+}
+
 
 createDivs();
+selectOption();
+
+
+
+
 
 
