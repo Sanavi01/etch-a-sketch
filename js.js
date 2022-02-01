ebe8d802c874dcbf09281d4c94be315c413813ac
Santiago Avila <sanavila2002@gmail.com>
2022-02-01 11:26:23 -0500

    Added function paintBlack, this function lets draw to the user just putting the mouse over the grid In css just was add the property to put the background of each div in black

--- js.js
+++ js.js
@@ -1,13 +1,9 @@
 const squareDivs = document.querySelector('.squareDivs');
 
-
 let quantify = prompt("De cuanto quieres que sea la cuadricula")
 
-
-
-
 function createDivs() {
-    let totalOfDivs = (quantify * quantify) - 1;
+    let totalOfDivs = (quantify * quantify) ;
     for (i = 0; i < `${totalOfDivs}`; i++) {
         const divs = document.createElement('div');
         divs.classList.add('gridDivs');
@@ -16,5 +12,16 @@ function createDivs() {
     squareDivs.setAttribute('style', `grid-template-columns: repeat(${quantify}, 1fr)`)
 }
 
+function paintBlack () {
+    const divsBlack = document.querySelectorAll('.gridDivs')
+    divsBlack.forEach(gridDivs => {
+        gridDivs.addEventListener('mouseover', () => {
+            gridDivs.classList.add('paintBlack')
+        })
+    })
+}
+
 createDivs();
+paintBlack();
+
 
