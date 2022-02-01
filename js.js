ef21401215e06083a7dbaaf50e0576966e4feed4
Santiago Avila <sanavila2002@gmail.com>
2022-02-01 12:07:31 -0500

    Added function rainbowColor This function set a random color in each gridDivs when the user put is mouse over each one

--- js.js
+++ js.js
@@ -21,7 +21,26 @@ function paintBlack () {
     })
 }
 
+function eraser () {
+    const divsBlack = document.querySelectorAll('.gridDivs')
+    divsBlack.forEach(gridDivs => {
+        gridDivs.addEventListener('mouseover', () => {
+            gridDivs.classList.remove('paintBlack')
+            gridDivs.classList.remove('rainbowColor')
+        })
+    })
+}
+
+function rainbowColor () {
+    const divsBlack = document.querySelectorAll('.gridDivs')
+    divsBlack.forEach(gridDivs => {
+        gridDivs.addEventListener('mouseover', () => {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            gridDivs.setAttribute('style' , `background-color: #${randomColor}`)
+        })
+    })
+}
+
 createDivs();
-paintBlack();
 
 
